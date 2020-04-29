da6d3952a0e90a6d460db28367d2dca601518825
Zia Shahizadeh <ziashahizadeh@gmail.com>
2020-04-29 09:43:54 +0430

    Completed Print Out of Receipt

---
+++ App.Application/Registration/Queries/GetApplicationOfficeQuery.cs
@@ -0,0 +1,48 @@
+﻿using App.Application.Registration.Models;
+using App.Persistence.Context;
+using Clean.Persistence.Identity;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace App.Application.Registration.Queries
+{
+    public class GetApplicationOfficeQuery : IRequest<SearchOfficeModel>
+    {
+        public int ID { get; set; }
+
+    }
+
+    public class GetApplicationOfficeQueryHandler : IRequestHandler<GetApplicationOfficeQuery, SearchOfficeModel>
+    {
+        private AppDbContext Context { get; }
+        private AppIdentityDbContext IdentityDbContext { get; }
+        public GetApplicationOfficeQueryHandler(AppDbContext context, AppIdentityDbContext identityDbContext)
+        {
+            Context = context;
+            IdentityDbContext = identityDbContext;
+        }
+        public async Task<SearchOfficeModel> Handle(GetApplicationOfficeQuery request, CancellationToken cancellationToken)
+        {
+            var query = Context.PassportApplications.Where(e => e.Id == request.ID).Single();
+
+            var user = IdentityDbContext.Users.Where(e => e.Id == query.CreatedBy).Single();
+
+            var office = await Context.Offices.Where(e => e.Id == user.OfficeID)
+                .Select(e => new SearchOfficeModel
+                {
+                    ID = e.Id,
+                    Name = e.Title,
+                    NameEn = e.TitleEn,
+                    Code = e.Code
+                }).SingleAsync();
+
+            return office;
+        }
+    }
+}
--- Clean.UI/Pages/Passport/Register/Prints/Index.cshtml.cs
+++ Clean.UI/Pages/Passport/Register/Prints/Index.cshtml.cs
@@ -13,14 +13,18 @@ namespace Clean.UI.Pages.Passport.Register.Prints
 {
     public class IndexModel : BasePage
     {
-        public string Barcode { get; set; }
+        public string ApplicationCode { get; set; }
+        public string ProfileCode { get; set; }
+
         public string ProfilePhoto { get; set; }
         public string ProfileSignature { get; set; }
 
-
+        public string PassportType { get; set; }
+        public string OfficeName { get; set; }
+        public string AmountPaid { get; set; }
+        public string PassportDuration { get; set; }
 
         public string Tazkira { get; set; }
-        public string TazkiraLocation { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
         public string FatherName { get; set; }
@@ -28,106 +32,36 @@ namespace Clean.UI.Pages.Passport.Register.Prints
         public string DoB { get; set; }
         public string BirthLocation { get; set; }
         public string Gender { get; set; }
-        public string MaritalStatus { get; set; }
-        public string BloodGroup { get; set; }
-        public string Ethnicity { get; set; }
-
-
-
-
-        public string M40Detail { get; set; }
-        public string CreatedOn { get; set; }
-
-
-
-
-        public string Rank { get; set; }
-        public string Salary { get; set; }
-        public string RankAfter { get; set; }
-        public string Orgnization { get; set; }
-        public string SalaryDate { get; set; }
-        public string Category { get; set; }
-
-        public string ApplicationReason { get; set; }
-        public string RegistrationType { get; set; }
-        public string EventReason { get; set; }
-        public string EventDate { get; set; }
-        public string EventLocation { get; set; }
-        public string EventDetails { get; set; }
-
-
-
-        public string FirstHeritageTazkira { get; set; }
-        public string FirstHeritageName { get; set; }
-        public string FirstHeritageLastName { get; set; }
-        public string FirstHeritageFatherName { get; set; }
-        public string FirstHeritageGrandFatherName { get; set; }
-        public string FirstHeritageRelation { get; set; }
-        public string FirstHeritageM40 { get; set; }
-        public string FirstHeritageGender { get; set; }
-
-        public string FirstHeritageCurrentProvince { get; set; }
-        public string FirstHeritageCurrentDistrict { get; set; }
-
-
-        public string FirstHeritageWasiqaNumber { get; set; }
-        public string FirstHeritageWasiqaLocation { get; set; }
-        public string FirstHeritageWasiqaDate { get; set; }
-        public string FirstHeritagePhoto { get; set; }
-        public string FirstHeritageMobile { get; set; }
-
-
-        public string LawyerPhoto { get; set; }
-        public string LawyerTazkira { get; set; }
-
-        public string LawyerName { get; set; }
-        public string LawyerFatherName { get; set; }
-        public string LawyerGrandFatherName { get; set; }
-        public string LawyerMobile { get; set; }
-
-
-
-        public string DateOfBirthShamsi { get; set; }
-
-
-        public string LawyerRegistrationNumber { get; set; }
-        public string LawyerRegistrationDate { get; set; }
-        public string LawyerRegistrationLocation { get; set; }
-
-
-        public string LawyerRegistrationCProvince { get; set; }
-        public string LawyerRegistrationCDistrict { get; set; }
-
-
-
-        public string LawyerType { get; set; }
-        public string AccountName { get; set; }
-        public string BankName { get; set; }
-        public string AccountNumber { get; set; }
+        public string CurrentCountry { get; set; }
         
-
-        public string Heritages { get; set; }
-
-        string tabletemplate = @"
-                            <tr>
-                            <td>$Name</td>
-                            <td>$Relation</td>
-                            <td>$DoB</td>
-                            <td>$Age</th>
-                            <td>$Tazkira</td>
-                            <td>$IsQualified</td>
-                        </tr>";
-
         public async Task OnGetAsync([FromQuery]int recordid)
         {
             var apps = await Mediator.Send(new SearchApplicationQuery { ID = recordid });
             var app = apps.First();
-            Barcode = app.Code;
+            ApplicationCode = app.Code;
             ProfilePhoto = await GetFile(AppConfig.ImagesPath, app.PhotoPath);
             ProfileSignature = await GetFile(AppConfig.SignaturesPath, app.SignaturePath);
+            AmountPaid = app.PaidAmount.ToString();
+            PassportDuration = app.PassportDuration + " Months";
+            PassportType = app.PassportType;
+            var office = await Mediator.Send(new GetApplicationOfficeQuery { ID = app.Id });
+            OfficeName = office.NameEn;
 
             var profiles = await Mediator.Send(new SearchProfileQuery { ID = app.ProfileId });
             var prf = profiles.First();
+
+            ProfileCode = prf.Code;
+            Name = prf.NameEn;
+            FatherName = prf.FatherNameEn;
+            LastName = prf.FamilyNameEn;
+            GrandFatherName = prf.GrandFatherNameEn;
+
+            CurrentCountry = prf.ResidenceCountryText;
+            DoB = prf.DateOfBirth;
+            BirthLocation = prf.BirthCountryText;
+            Gender = prf.GenderCode;
+            Tazkira = prf.NIDSerial;
+
 
         }
 
