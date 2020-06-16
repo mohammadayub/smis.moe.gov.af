bb234d7c4331950a13b8b234febec04d77e48e6e
Mohammad Ayub Yousafzai <mohammadayub864@gmail.com>
2020-06-16 13:25:40 +0430

    Added NID Info
    Added NID Information in Student Profile

--- Clean.UI/Pages/Student/Profile/Profile.cshtml.cs
+++ Clean.UI/Pages/Student/Profile/Profile.cshtml.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using App.Application.Lookup.Queries;
 using App.Domain.Entity.look;
+using Castle.Core.Configuration;
+using Clean.Application.Documents.Queries;
 using Clean.Application.System.Queries;
+using Clean.Common;
+using Clean.Common.Enums;
+using Clean.Common.Models;
+using Clean.Common.Storage;
 using Clean.Persistence.Services;
 using Clean.UI.Types;
 using Clean.UI.Utilities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -19,8 +26,7 @@ namespace Clean.UI.Pages.Student.Profile
     //[Authorize(Roles = "Administrator,Data-Entry")]
     public class ProfileModel : BasePage
     {
-
-     
+       
         public string SubScreens { get; set; }
         private string htmlTemplate = @"
                          <li><a href='#' data='$id' page='$path' class='sidebar-items' action='subscreen'><i class='$icon'></i>$title</a></li>";
@@ -57,10 +63,10 @@ namespace Clean.UI.Pages.Student.Profile
             string Screen = EncryptionHelper.Decrypt(HttpContext.Request.Query["p"]);
             int ScreenID = Convert.ToInt32(Screen);
 
-            //ListOfDocumentTypes = new List<SelectListItem>();
-            //var documentTypes = await Mediator.Send(new GetDocumentTypeQuery() { ScreenID = ScreenID, Catagory = "ID" });
-            //foreach (var documentType in documentTypes)
-            //    ListOfDocumentTypes.Add(new SelectListItem() { Text = documentType.Name, Value = documentType.Id.ToString() });
+            ListOfDocumentTypes = new List<SelectListItem>();
+            var documentTypes = await Mediator.Send(new GetDocumentTypeQuery() { ScreenID = ScreenID, Catagory = "ID" });
+            foreach (var documentType in documentTypes)
+                ListOfDocumentTypes.Add(new SelectListItem() { Text = documentType.Name, Value = documentType.Id.ToString() });
 
             try
             {
@@ -81,5 +87,78 @@ namespace Clean.UI.Pages.Student.Profile
 
 
         }
+
+
+
+        public async Task<IActionResult> OnPostProvince([FromBody] DynamicListModel Data)
+        {
+            var result = new JsonResult(null);
+            try
+            {
+                List<object> SearchResult = new List<object>();
+                var location = await Mediator.Send(new GetLocationList() { ID = Data.ID });
+                foreach (var l in location)
+                    SearchResult.Add(new { id = l.Id, text = l.Dari });
+
+                return new JsonResult(new UIResult()
+                {
+                    Data = new { list = SearchResult },
+                    Status = UIStatus.Success,
+                    Text = "",
+                    Description = string.Empty
+                });
+
+            }
+            catch (Exception ex)
+            {
+                result = new JsonResult(CustomMessages.FabricateException(ex));
+            }
+            return result;
+        }
+
+
+
+        public async Task<IActionResult> OnPostUpload([FromForm] IFormFile img, [FromForm] string UploadType)
+        {
+            FileStorage _storage = new FileStorage();
+            var extension = System.IO.Path.GetExtension(img.FileName);
+            // check for a valid mediatype
+            if (!img.ContentType.StartsWith("image/"))
+            {
+                return new JsonResult(new UIResult()
+                {
+                    Data = null,
+                    Status = 0,
+                    Text = "فارمت عکس درست نیست",
+                    // Can be changed from app settings
+                    Description = ""
+                });
+            }
+            else
+            {
+                string basePath = "";
+                if (UploadType == UploadTypes.Photo)
+                {
+                    basePath = AppConfig.ImagesPath;
+                }
+                else if (UploadType == UploadTypes.Signature)
+                {
+                    basePath = AppConfig.SignaturesPath;
+                }
+                var additional = DateTime.Now.ToString("yyyy-MM-dd") + "\\";
+                string filename = await _storage.CreateAsync(img.OpenReadStream(), extension, basePath + additional);
+                var result = new
+                {
+                    status = "success",
+                    url = additional + filename
+                };
+                return new JsonResult(result);
+            }
+        }
+
+
+
+
+
     }
 }
